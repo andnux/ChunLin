5df05f054bcd1e7f2d9d7e7d0c7a1a7ed419c894
张春林 <1018827187@qq.com>
2019-07-02 21:26:33 +0800

    添加皮肤支持

--- skin/src/main/java/top/andnux/skin/BaseSkinActivity.java
+++ skin/src/main/java/top/andnux/skin/BaseSkinActivity.java
@@ -1,118 +1,28 @@
 package top.andnux.skin;
 
 
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewParent;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.LayoutInflaterCompat;
 import androidx.core.view.LayoutInflaterFactory;
-import androidx.core.view.ViewCompat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import top.andnux.skin.attr.SkinAttr;
-import top.andnux.skin.attr.SkinView;
 import top.andnux.skin.callback.ISkinChangeListener;
-import top.andnux.skin.support.SkinAppCompatViewInflater;
-import top.andnux.skin.support.SkinAttrSupport;
 
-public abstract class BaseSkinActivity extends AppCompatActivity implements LayoutInflaterFactory, ISkinChangeListener {
+public abstract class BaseSkinActivity extends AppCompatActivity
+        implements LayoutInflaterFactory, ISkinChangeListener {
 
-    // 后面会写插件换肤 预留的东西
-    private SkinAppCompatViewInflater mAppCompatViewInflater;
-
-    private String TAG = "BaseSkinActivity";
+    private SkinFactory mFactory;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        LayoutInflaterCompat.setFactory(layoutInflater, this);
+        mFactory = new SkinFactory(this, this);
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-
-        // 拦截到View的创建  获取View之后要去解析
-        // 1. 创建View
-        // If the Factory didn't handle it, let our createView() method try
-        View view = createView(parent, name, context, attrs);
-
-        // 2. 解析属性  src  textColor  background  自定义属性
-        // Log.e(TAG, view + "");
-
-        // 2.1 一个activity的布局肯定对应多个这样的 SkinView
-        if (view != null) {
-            List<SkinAttr> skinAttrs = SkinAttrSupport.getSkinAttrs(context, attrs);
-            SkinView skinView = new SkinView(view, skinAttrs);
-            // 3.统一交给SkinManager管理
-            managerSkinView(skinView);
-
-            // 4.判断一下要不要换肤
-            SkinManager.getInstance().checkChangeSkin(skinView);
-        }
-        return view;
-    }
-
-    /**
-     * 统一管理SkinView
-     */
-    private void managerSkinView(SkinView skinView) {
-        List<SkinView> skinViews = SkinManager.getInstance().getSkinViews(this);
-        if (skinViews == null) {
-            skinViews = new ArrayList<>();
-            SkinManager.getInstance().register(this, skinViews);
-        }
-        skinViews.add(skinView);
-    }
-
-
-    public View createView(View parent, final String name, @NonNull Context context,
-                           @NonNull AttributeSet attrs) {
-        final boolean isPre21 = Build.VERSION.SDK_INT < 21;
-
-        if (mAppCompatViewInflater == null) {
-            mAppCompatViewInflater = new SkinAppCompatViewInflater();
-        }
-
-        // We only want the View to inherit it's context if we're running pre-v21
-        final boolean inheritContext = isPre21 && true
-                && shouldInheritContext((ViewParent) parent);
-
-        return mAppCompatViewInflater.createView(parent, name, context, attrs, inheritContext,
-                isPre21, /* Only read android:theme pre-L (L+ handles this anyway) */
-                true /* Read read app:theme as a fallback at all times for legacy reasons */
-        );
-    }
-
-    private boolean shouldInheritContext(ViewParent parent) {
-        if (parent == null) {
-            // The initial parent is null so just return false
-            return false;
-        }
-        while (true) {
-            if (parent == null) {
-                return true;
-            } else if (parent == getWindow().getDecorView() || !(parent instanceof View)
-                    || ViewCompat.isAttachedToWindow((View) parent)) {
-                return false;
-            }
-            parent = parent.getParent();
-        }
-    }
-
-    @Override
     protected void onDestroy() {
-        SkinManager.getInstance().unregister(this);
+        mFactory.onDestroy();
         super.onDestroy();
     }
 }
